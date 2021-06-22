1b54a493cf26c94ad1a748c0689d367572096a0d
jaufmann <waldemar.jaufmann@bcmsolutions.de>
2021-06-22 19:37:20 +0200

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,26 @@
-import {AfterViewInit, ChangeDetectorRef, Component, HostListener, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, HostListener, ViewChild} from '@angular/core';
 import {fromEvent, Subject} from "rxjs";
 import {takeUntil} from "rxjs/operators";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, AfterViewInit{
-  title = 'waldinvest';
-
-  @ViewChild('sidenav') sidenav;
-    @ViewChild('sidenavvvvv') sidenavvvvv;
+export class AppComponent implements AfterViewInit{
+    @ViewChild('sidenav') sidenav;
+    @ViewChild('sideSectionRight') sideSectionRight;
 
     public fullFooter = false;
 
     destroy = new Subject();
-
     destroy$ = this.destroy.asObservable();
 
-    constructor() {
+    constructor(private router: Router) {
         fromEvent(window, 'scroll').pipe(takeUntil(this.destroy$))
             .subscribe((e: Event) => console.log(this.getYPosition(e)));
     }
-
-    ngOnInit() {}
 
     ngAfterViewInit() {
         if (window.innerWidth <= 995) {
@@ -33,10 +29,8 @@ export class AppComponent implements OnInit, AfterViewInit{
     }
 
     @HostListener('window:resize', ['$event'])
-    onResize(event?) {
-        console.log(this.sidenavvvvv.nativeElement.offsetTop);
-
-        this.fullFooter = this.sidenavvvvv.nativeElement.offsetTop !== 300;
+    onResize() {
+        this.fullFooter = this.sideSectionRight.nativeElement.offsetTop !== 300;
 
         if (window.innerWidth <= 959) {
             if (this.sidenav._animationState === 'open') {
@@ -47,17 +41,12 @@ export class AppComponent implements OnInit, AfterViewInit{
         }
     }
 
-
-    @HostListener('window:scroll', ['$event'])
-    onScroll(e: Event): void {
-        console.log(e);
+    async navigateAndCloseNav(type: string) {
+        this.sidenav.toggle();
+        await this.router.navigate([type]);
     }
 
     getYPosition(e: Event): number {
         return (e.target as Element).scrollTop;
-    }
-
-    next() {
-
     }
 }
